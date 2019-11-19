87e026ce82829d83a4b64e9699b1814adb74cff7
Eric Miao <miaoyx@shanghaitech.edu.cn>
2019-11-19 11:57:08 +0800

    start implementing frame.c frame.h. Using list to implement frame table

--- Zf_proj/vm/frame.c
+++ Zf_proj/vm/frame.c
@@ -2,9 +2,34 @@
 #include <stdio.h>
 #include <string.h>
 #include "threads/malloc.h"
-#include "threads/synch.h"
 #include "threads/palloc.h"
-#include "lib/kernel/list.h"
 #include "userprog/pagedir.h"
 #include "threads/pte.h"
 #include "vm/frame.h"
+
+/* Use list to implement frame table, nodes as entries. */
+static struct list frame_table;
+
+/* Lock for accessing the frame table */
+static struct lock frame_table_lock;
+
+/* The hand for eviction clock. */
+static size_t clock_hand;
+
+/* Initialize the whole frame system. */
+void frame_init()
+{
+    list_init (&frame_table);
+    lock_init (&frame_table_lock);
+
+    void* phys_mem = palloc_get_page(PAL_USER);
+    while (phys_mem != NULL)
+    {
+        struct frame *new_frame = malloc(sizeof(stuct frame));
+        new_frame->ker_base = phys_mem;
+        new_frame->page = NULL;
+        lock_init (new_frame->frame_lock);
+        list_push_back (&frame_table, &new_frame->frame_elem);
+        phys_mem = palloc_get_page(PAL_USER);
+    }
+}
--- Zf_proj/vm/frame.h
+++ Zf_proj/vm/frame.h
@@ -2,3 +2,21 @@
 #define VM_FRAME_H
 
 #include "threads/thread.h"
+#include "lib/kernel/list.h"
+#include "threads/synch.h"
+#include "vm/pages.h"
+
+#include <stdbool.h>
+struct frame
+{
+    struct lock frame_lock;     /* The lock ensures critical section. */
+    void* ker_base;                 /* Kernel virtual page. */
+    struct page *page;          /* The page this frame assigned to. */
+    struct list_elem fte        /* Frame table entry. */
+};
+
+void* frame_init(void);
+struct frame* frame_allocate (struct page *);
+bool frame_free (struct frame *);
+bool frame_lock (struct frame*);
+bool frame_unlook (struct frame*);
