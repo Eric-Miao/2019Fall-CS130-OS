1de3b8813706e1834487277624a45a6ed3b04491
Eric Miao <miaoyx@shanghaitech.edu.cn>
2019-11-19 21:16:21 +0800

    I think I shoudl have finished 80% of frame.c and frame.h. Need to do optimization and need to finish page in order to do the following work. BTW the evict is written but commented out for debugging, remember to uncomment it before using.

--- Zf_proj/vm/frame.c
+++ Zf_proj/vm/frame.c
@@ -13,8 +13,8 @@ static struct list frame_table;
 /* Lock for accessing the frame table */
 static struct lock frame_table_lock;
 
-/* The hand for eviction clock. */
-static size_t clock_hand;
+/* The loop indicator for eviction clock. */
+static size_t clock_loop;
 
 /* Initialize the whole frame system. */
 void 
@@ -65,8 +65,14 @@ frame_unlock(struct frame *f)
     lock_release(&f->frame_lock);
 }
 
+/* ACTUALLY, I think this implementation is kind of silly because it travels the 
+    frame table to determine if the frame is owned by the current thread.
+    Rather than go through the process's page table to see what frame he gets,
+    and choose from them.
+    Or use another method to show if the frame is owned by the process.
+    Just suggestions. */
 struct frame*
-frame_allocate(struct page * p)
+frame_allocate(struct page *p)
 {
     lock_acquire(&frame_table_lock);
     
@@ -76,8 +82,13 @@ frame_allocate(struct page * p)
     for (e = list_begin(l); e != list_end(l); e = list_next(e))
     {
         struct frame *f = list_entry(e, struct frame, fte);
+        
+        /* If the current thread doesn't have the lock of this frame
+            This means the this frame doesn't belong to the current process,
+            just go on to check the next frame. */
         if (!lock_try_acquire(&f->frame_lock))
             continue;
+        
         if (f->page == NULL)
         {
             f->page = page;
@@ -86,6 +97,74 @@ frame_allocate(struct page * p)
         }
         lock_release(&f->frame_lock);
     }
+    
+    /*  If reach here, no free frame currently, start to evict. 
+        For now, just panic. evict shall be on at last.*/
+    PANIC("no enough frames to allocate.");
+    //return frame_evict(p);
+}
 
-    /* If reach here, no free frame currently, start to evict. */
+/* Use the clock policy to choose a frame to evict.
+   The hand is the right now pointer. 
+   Go through the list twice. 
+   For the first time, find a frame to evict. 
+   For the second time, locate the free frame and return.
+   Evict the frame whose page is not recently accessed. 
+   IF evict fails return NULL;*/
+static struct frame*
+frame_evict(struct page *p)
+{
+    struct list_elem *e;
+    struct list *l = &frame_table;
+    clock_loop = 1;
+
+    for (e = list_begin(l); (e != list_end(l) || clock_loop < 3); e = list_next(e))
+    {
+        /* When reach the tail, start from the head again and loop ++. */
+        if (e == list_end(l))
+        {
+            clock_loop ++;
+            e = list_begin(l);
+        }
+
+        struct frame *f = list_entry(e, struct frame, fte);
+        if (!lock_try_acquire(&f->frame_lock))
+            continue;
+            
+        if (f->page == NULL)
+        {
+            f->page = page;
+            lock_release(&frame_table_lock);
+            /* The page is mine now, no need to release the lock. */
+            return f;
+        }
+
+        /*  If the page has recently accessed, clean the access bit and continue. 
+            IMPORTANT: CLEAR THE ACCESS BIT IN PAGE P AFTER I CHECK.*/
+        if (page_recently_accessed(p))
+        {
+            lock_release(&f->frame_lock);
+            continue;
+        }
+
+        /* If the page is not NULL | not recently accessed  
+            try to evivt it and return the frame if success.
+            Otherwise return NULL */
+        if (page_out(p))
+        {
+            lock_release(&f->frame_table_lock);
+            /* The page is mine now, no need to release the lock. */
+            f->page = page;
+            return f;
+        }
+        /* page out fail */
+        else
+        {
+            lock_release(&frame_table_lock);
+            lock_release(&f->frame_lock);
+            return NULL;
+        }
+    }
+    lock_release(&frame_table_lock);
+    return NULL;
 }
