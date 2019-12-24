1e83bfc2944541a1e6549464c495892931f0ae5e
Eric Miao <miaoyx@shanghaitech.edu.cn>
2019-12-25 02:03:06 +0800

    need to  modify the new inode interface in filesys.c and clearify the usage of . and .. and re-format some usage

--- proj4/filesys/filesys.c
+++ proj4/filesys/filesys.c
@@ -6,7 +6,9 @@
 #include "filesys/free-map.h"
 #include "filesys/inode.h"
 #include "filesys/directory.h"
-
+#include "filesys/cache.h"
+#include "threads/malloc.h"
+#include "threads/thread.h"
 /* Partition that contains the file system. */
 struct block *fs_device;
 
@@ -37,6 +39,7 @@ void
 filesys_done (void) 
 {
   free_map_close ();
+  cache_clear();
 }
 
 /* Creates a file named NAME with the given INITIAL_SIZE.
@@ -47,14 +50,23 @@ bool
 filesys_create (const char *name, off_t initial_size) 
 {
   block_sector_t inode_sector = 0;
-  struct dir *dir = dir_open_root ();
+  struct dir *dir;
+  char* filename;
+
+  int open_status = parse_path(name, &dir, &filename);
+
+  if (open_status != 1)
+    return false;
+
   bool success = (dir != NULL
                   && free_map_allocate (1, &inode_sector)
-                  && inode_create (inode_sector, initial_size)
-                  && dir_add (dir, name, inode_sector));
+                  && inode_create (inode_sector, initial_size, false)
+                  && dir_add (dir, name, inode_sector, false));
   if (!success && inode_sector != 0) 
     free_map_release (inode_sector, 1);
+
   dir_close (dir);
+  free(filename);
 
   return success;
 }
@@ -68,12 +80,20 @@ struct file *
 filesys_open (const char *name)
 {
   struct dir *dir = dir_open_root ();
+  char *filename;
   struct inode *inode = NULL;
+
+  int result = parse_pathname(name, &dir, &filename);
+  if (result == -1)
+    return false;
+  if (result == 2)
+    return file_open(inode_open(ROOT_DIR_SECTOR));
 
   if (dir != NULL)
     dir_lookup (dir, name, &inode);
   dir_close (dir);
 
+  free(filename);
   return file_open (inode);
 }
 
@@ -84,9 +104,26 @@ filesys_open (const char *name)
 bool
 filesys_remove (const char *name) 
 {
-  struct dir *dir = dir_open_root ();
-  bool success = dir != NULL && dir_remove (dir, name);
-  dir_close (dir); 
+  struct dir *dir;
+  char *filename;
+  int result = parse_pathname(name, &dir, &filename);
+  bool success;
+
+  /* Root dir cannot be removed and if fail parsing */
+  if (result == -1 || result == 2)
+    return false;
+
+  /* In case there are . and .. */
+  if (!strcmp(filename, ".") || !strcmp(filename, ".."))
+  {
+    dir_close(dir);
+    free(filename);
+    return false;
+  }
+
+  bool success = dir != NULL && dir_remove(dir, name);
+  dir_close(dir);
+  free(filename);
 
   return success;
 }
@@ -99,6 +136,122 @@ do_format (void)
   free_map_create ();
   if (!dir_create (ROOT_DIR_SECTOR, 16))
     PANIC ("root directory creation failed");
+  struct dir *dir = dir_open_root();
+  dir_add(dir, ".", ROOT_DIR_SECTOR, true);
+  dir_add(dir, "..", ROOT_DIR_SECTOR, true);
+  dir_close(dir);
   free_map_close ();
   printf ("done.\n");
+}
+
+/* parse the filename from the pathname.
+  Store the parsed name into filename.
+  Store the opeded inode of dir in dir if the pathname is a dir.
+  return value -1:fail; 0:file; 1:dir; 2:root dir */
+int 
+parse_path(const char *pathname, struct dir **dir, char **filename)
+{
+  int ret;
+  char *path_buffer;
+  char *ptr_last;
+  char *token, *save_ptr;
+  bool if_absolute;
+  bool if_dir;
+  struct dir *cwd;
+
+  *dir = NULL;
+  *filename = NULL;
+
+  size_t len = strlen(pathname);
+  /* invalid name: empty name */
+  if (len == 0)
+  {    
+    ret = -1;
+    goto done;
+  }
+
+  path_buffer = malloc(len + 1);
+  /* invalid operations: malloc fail */
+  if (!path_buffer)
+  {
+    ret = -1;
+    goto done;
+  }
+  memcpy(path_buffer, pathname, len+1);
+
+  /* If a path begins with '/', it's a absolute addr. */
+  if_absolute = (*path_buffer == '/');
+  /* dir_cwd_removed check if current cwd exists.
+    which is only a problem with relative path because 
+    it depends on the relative path to locate the file.*/
+  if (!if_absolute && dir_cwd_removed())
+  {
+    free(path_buffer);
+    ret = -1;
+    goto done;
+  }
+
+  ptr_last = path_buffer + len - 1;
+  /* If the path ends with a '/', it's a dir rather than
+    a file. */
+  if_dir = (*ptr_last == '/');
+  while (ptr_last > path_buffer && *ptr_last == '/')
+    ptr_last--;
+  /* Go all the way to the begin point of the pathname
+    If the whole path is ///////, this is a root dir.
+    Otherwise the last points at the last not / position's / behind that. */
+  if (ptr_last == '/')
+  {
+    free(path_buffer);
+    ret = 2;
+    goto done;
+  }
+
+  /* Now it's time to abstract the filename */
+  /* This set the prt to become a char string. */
+  *(ptr_last + 1)= '\0';
+  while (ptr_last >= path_buffer && *ptr_last != '/')
+    --ptr_last;
+  ++ptr_last;
+  /* Now the ptr shall stop at the beginning position of file name.*/
+
+  size_t len_filename = strlen(ptr_last);
+  /* For bebug, check the len_filename > 0. */
+  ASSERT(len_filename > 0);
+
+  cwd = if_absolute ? dir_open_root() : dir_open_cwd();
+  for (token = strtok_r(path_buffer, "/", &save_ptr);
+       token != ptr_last && token != NULL;
+       token = strtok_r(NULL, "/", &save_ptr))
+  {
+    struct inode *inode;
+    /* Check if we have the file named token in cwd. */
+    if (!dir_lookup(cwd, token, &inode))
+    {
+      dir_close(cwd);
+      free(path_buffer);
+      ret = -1;
+      goto done;
+    }
+    dir_close(cwd);
+    cwd = dir_open(inode);
+  }
+  *dir = cwd;
+
+  *filename = malloc(len_filename + 1);
+  if (!*filename)
+  {
+    free(path_buffer);
+    ret -1;
+    goto done;
+  }
+
+  /* extracted filename stored into *filename */
+  memcpy(*filename, ptr_last, len_filename + 1);
+  free(path_buffer);
+  ret = 1 ? if_dir : 0;
+  goto done;
+
+  done:
+    return ret;
 }
