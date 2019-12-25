a6f5629bed64db1df2dc7ae194c4cc069c3764ce
zhangfan <841319104@qq.com>
2019-12-25 21:45:20 +0800

    -config

--- proj4/filesys/cache.c
+++ proj4/filesys/cache.c
@@ -1,34 +1,8 @@
 #include <string.h>
 #include <stdbool.h>
 #include <debug.h>
-#include "threads/synch.h"
-#include "threads/thread.h"
-#include "threads/malloc.h"
-#include "devices/timer.h"
-#include "filesys/filesys.h"
 #include "filesys/cache.h"
 #define MAX_CACHE 64
-
-struct cache_line
-{
-    uint8_t data[BLOCK_SECTOR_SIZE]; /*data in this cache line*/
-    block_sector_t sector;           /*the sector that cache line has*/
-    int waiters;                     /*number of users that is waiting for reading/writing from/to this cache line*/
-    int indicator;                   /*indicator that shows whether cache line is exclusive or not*/
-    struct lock cache_line_lock;     /*lock to protect data in cache line*/
-    struct lock bool_lock;           /*lock to protect all bool indicator variables*/
-    struct condition waiting_queue;  /*readers/writers wating in this condition variable to avoid racing*/
-    bool accessed;                   /*whether the cache line is accessed (for eviction)*/
-    bool dirty;                      /*whether the cache line is modified*/
-    bool used;                       /*whether the cache line is used*/
-};
-
-/*readhead element*/
-struct ahead
-{
-    block_sector_t sector; /*read head sector*/
-    struct list_elem elem; /*store it in the list*/
-};
 
 /*create the cache*/
 struct cache_line cache[MAX_CACHE];
--- proj4/filesys/cache.h
+++ proj4/filesys/cache.h
@@ -2,6 +2,32 @@
 #define FILESYS_CACHE_H
 
 #include "devices/block.h"
+#include "threads/synch.h"
+#include "threads/thread.h"
+#include "threads/malloc.h"
+#include "devices/timer.h"
+#include "filesys/filesys.h"
+
+struct cache_line
+{
+    uint8_t data[BLOCK_SECTOR_SIZE]; /*data in this cache line*/
+    block_sector_t sector;           /*the sector that cache line has*/
+    int waiters;                     /*number of users that is waiting for reading/writing from/to this cache line*/
+    int indicator;                   /*indicator that shows whether cache line is exclusive or not*/
+    struct lock cache_line_lock;     /*lock to protect data in cache line*/
+    struct lock bool_lock;           /*lock to protect all bool indicator variables*/
+    struct condition waiting_queue;  /*readers/writers wating in this condition variable to avoid racing*/
+    bool accessed;                   /*whether the cache line is accessed (for eviction)*/
+    bool dirty;                      /*whether the cache line is modified*/
+    bool used;                       /*whether the cache line is used*/
+};
+
+/*readhead element*/
+struct ahead
+{
+    block_sector_t sector; /*read head sector*/
+    struct list_elem elem; /*store it in the list*/
+};
 
 void cache_init(void);
 struct cache_line *cache_allocate(block_sector_t sector, bool exclusive);
